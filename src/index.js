e7a2630960cd45a6d5605d8a6a1b5644612fa681
Banjo Tola <banjotola12@gmail.com>
2021-12-14 19:55:44 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Contact from './screens/Contact';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Contact />
+  
   </React.StrictMode>,
   document.getElementById('root')
 );
