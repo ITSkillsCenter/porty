8da0091a3c368fcfebb7ab278455b2af4058123a
Viashima <72166830+clins10@users.noreply.github.com>
2021-12-14 19:19:41 +0100

    mywork

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Tst from './Tst';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+     <App /> 
+    {/* <Tst /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
