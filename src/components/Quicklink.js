311cdef800d224d77bd67c4d0d1cb791e5ead0a1
Viashima <72166830+clins10@users.noreply.github.com>
2021-12-14 19:29:01 +0100

    mywrk

--- src/components/Quicklink.js
+++ src/components/Quicklink.js
@@ -3,7 +3,7 @@ import Link from 'react'
 
 function Quicklink() {
 	return (
-		<div className="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12 mb-4">
+		
 			<div className="p-5 tm-bg-gray">
 				<h3 className="tm-text-primary mb-4">Quick Links</h3>
 				<ul className="list-unstyled tm-footer-links">
@@ -24,9 +24,8 @@ function Quicklink() {
 					</li>
 				</ul>
 			</div>
-		</div>
+		
 	);
 }
 
 export default Quicklink;
-o
